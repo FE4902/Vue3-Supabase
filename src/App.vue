6dc3a1388afa5709fe7996407682e38068d2dfa9
FE4902 <isdn000123@gmail.com>
2024-11-20 22:06:57 +0900

    init project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+    <div>
+        <h1>Vue3 Supabase</h1>
+    </div>
+</template>
+
+<style scoped lang="scss">
+div {
+    h1 {
+        color: red;
+
+        &:hover {
+            color: blue;
+        }
+    }
+}
+</style>
